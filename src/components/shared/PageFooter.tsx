4667bf48ab089b1445751d4a4615558d7b14e42a
Luis Alonso Vargas <luis.vargas@trulyengaging.com>
2023-12-20 12:25:27 -0600

    feat: initial commit

---
+++ src/components/shared/PageFooter.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material';
+import * as React from 'react';
+
+const PageFooter = () => {
+  return (
+    <section>
+      <Box sx={{ m: 2, textAlign: 'center', fontSize: '0.8rem' }}>
+        Some Footer for sample
+      </Box>
+    </section>
+  );
+};
+
+export default PageFooter;
