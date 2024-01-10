0987e11b3da3124964ae7d1520513991394ea869
Luis Alonso Vargas <luis.vargas@trulyengaging.com>
2024-01-10 12:53:48 -0600

    fix: remove old sample pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Container, ThemeProvider } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import * as React from 'react';
 
-import '@/styles/general.scss'; // Adjust the path accordingly
-
 import MainAppBar from '@/components/shared/MainAppBar/MainAppBar';
 import SideIconMenu from '@/components/shared/SideIconMenu/SideIconMenu';
 import StyleStepper from '@/components/shared/StyleStepper/StyleStepper';
